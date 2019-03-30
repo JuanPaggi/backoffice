0e19b018024b01ed5cdef3c09a24deda9c024b0d
Alexander Eberle <alexander171294@gmail.com>
2019-03-30 12:25:27 -0300

    router ajax

--- src/routes.php
+++ src/routes.php
@@ -9,7 +9,7 @@ _::declare_controller('positions', 'positions');
 _::declare_controller('configs', 'configs');
 
 // peticiones ajax
-_::declare_controller('users', 'jx_user_unlock', 'jx_user_rem_admin', 'jx_user_add_admin', 'jx_user_delete');
+_::declare_controller('users', 'jx_user_unlock', 'jx_user_rem_admin', 'jx_user_add_admin', 'jx_user_delete', 'jx_user_do_premium', 'jx_user_rem_premium');
 _::declare_controller('events', 'jx_event_delete', 'jx_event_delete_stand', 'jx_event_delete_code', 'jx_event_codegen');
 
 _::declare_controller('tst', 'tst');
